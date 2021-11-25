9ebd1f3814d97ef40e0c3546ed4555c2e8c6e5be
Aron Lee Freyer <aron-freyer@gmx.de>
2021-11-25 22:42:40 +0100

    Finish day 8

---
+++ 08_py/08_02/08_02.py
@@ -0,0 +1,39 @@
+import re
+
+registers = {}
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+absolutMax = 0
+
+for line in lines:
+    x = re.search("(.+) (.+) ([-0-9]+) if (.+) ([<>=!]+) ([-0-9]+)", line)
+
+    targetRegister = x.group(1)
+    action = x.group(2)
+    value = int(x.group(3))
+
+    conditionalRegister = x.group(4)
+    compareAction = x.group(5)
+    compareValue = int(x.group(6))
+
+    if targetRegister not in registers.keys():
+        registers[targetRegister] = 0
+
+    trueValue = 0
+    if conditionalRegister in registers.keys():
+        trueValue = registers[conditionalRegister]
+
+    comparisonSuccessful = eval(str(trueValue) + " " + compareAction + " " + str(compareValue))
+
+    if comparisonSuccessful:
+        if action == "inc":
+            registers[targetRegister] += value
+        else:
+            registers[targetRegister] -= value
+
+        if registers[targetRegister] > absolutMax:
+            absolutMax = registers[targetRegister]
+
+print(absolutMax)
